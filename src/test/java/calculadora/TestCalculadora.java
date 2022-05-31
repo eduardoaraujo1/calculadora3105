82495b3143db335a2edc5dcbf85b7407beab580d
Eduardo Araujo <eduardoas20@hotmail.com>
2022-05-31 20:19:54 -0300

    first commit

---
+++ src/test/java/calculadora/TestCalculadora.java
@@ -0,0 +1,16 @@
+package calculadora;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+public class TestCalculadora {
+    
+    
+   @Test
+   public void testGetAdicao() {
+       Calculadora calculadora = new Calculadora(4.0, 2.0);
+       double retornoEsperado = 6.0;
+       double retornoFeito = calculadora.getAdicao();
+       assertEquals(retornoEsperado, retornoFeito, 0);
+   }
+}
